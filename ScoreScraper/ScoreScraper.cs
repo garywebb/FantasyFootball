6d7b65f232f94a208cb2a25ab8facf894b33be53
Gary Webb <gary@thecloudandcompass.com>
2020-01-25 21:39:36 +0000

    Added storage of output to blob storage

--- ScoreScraper/ScoreScraper.cs
+++ ScoreScraper/ScoreScraper.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.Storage;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -15,6 +16,7 @@ namespace ScoreScraper
         [FunctionName("ScoreScraper")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest request,
+            [Queue("playerDataQueue"), StorageAccount("AzureWebJobsStorage")] ICollector<string> msg,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -24,6 +26,11 @@ namespace ScoreScraper
             string requestBody = await new StreamReader(request.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+
+            if (!String.IsNullOrEmpty(name))
+            {
+                msg.Add($"Name passed to the function: {name}");
+            }
 
             return name != null
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
